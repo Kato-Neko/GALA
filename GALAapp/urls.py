dd417d853c6e0f40f829cbe7691b440180d84290
Kato-Neko <ng112403@gmail.com>
2024-11-19 19:41:27 +0800

    git rehaul

--- GALAapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from django.contrib.auth.views import LoginView
-
-urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('auth/', include('django.contrib.auth.urls')),
-]
