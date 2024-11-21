e7562fb32b8fe1fc2f4e9c35a3c759333797e14c
Kato-Neko <ng112403@gmail.com>
2024-11-21 23:23:02 +0800

    merged nekato and maggins

--- Gala/Gala/urls.py
+++ Gala/Gala/urls.py
@@ -21,9 +21,5 @@ from recommendation import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
-    path('recommendation/', views.recommendation_view, name='recommendation'),
-    path('admin-location/', views.admin_location_view, name='admin_location'),
-    path('add-location/', views.add_location, name='add_location'),
-    path('edit-location/<int:pk>/', views.edit_location, name='edit_location'),
-    path('delete-location/<int:pk>/', views.delete_location, name='delete_location'),
+    path('recommendation/', include('recommendation.urls')),
 ]
