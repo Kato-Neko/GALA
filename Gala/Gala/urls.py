41d675e63c293b6454e65e202784f9b3c8be4b24
Kato-Neko <ng112403@gmail.com>
2024-11-21 23:46:47 +0800

    changes to urls.py

--- Gala/Gala/urls.py
+++ Gala/Gala/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
     path('recommendation/', include('recommendation.urls')),
+    path('calendar/', include('calendar.urls')),
 ]
