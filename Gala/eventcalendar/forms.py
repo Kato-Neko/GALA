550fda20b9bf81ec3ba95768f5f1d9590e03904a
Maggins08 <165860874+Maggins08@users.noreply.github.com>
2024-12-06 01:25:07 +0800

    Changes in uploading images
    Nagbutang ko sa database kanang makaadd ug image para sa eventcalendar

---
+++ Gala/eventcalendar/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import EventReminder
+
+class EventReminderForm(forms.ModelForm):
+    class Meta:
+        model = EventReminder
+        fields = ['description', 'start_time', 'end_time', 'date', 'longitude', 'latitude']  # Exclude 'image'
--- Gala/eventcalendar/models.py
+++ Gala/eventcalendar/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class EventReminder(models.Model):
-    event_reminder_id = models.AutoField(primary_key=True)  
+    event_reminder_id = models.AutoField(primary_key=True)
     description = models.CharField(max_length=200)
     start_time = models.TimeField()
     end_time = models.TimeField()
     date = models.DateField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
-    latitude = models.FloatField(null=True, blank=True)       
+    latitude = models.FloatField(null=True, blank=True)
+    image = models.ImageField(upload_to='reminder_images/', null=True, blank=True)  # Add this field
 
     def __str__(self):
         return self.description
